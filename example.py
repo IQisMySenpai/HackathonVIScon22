2f1e6eb91fb0a4ac33f605630a6081bfe5f02c04
Emilis Gužys <emilis.guzys@yahoo.com>
2022-10-14 19:15:52 +0200

    + Emilis first chanes

--- example.py
+++ example.py
@@ -3,6 +3,6 @@ from mongo_api import MongoAPI
 if __name__ == "__main__":
     mongo = MongoAPI('vvzpp.5byhvi1.mongodb.net', 'VVZpp', 'server', 'Xr9wqtrvnlufZQho')
 
-
+print("I'm fucking Emilis")
 
 print("Alex did some stuff which is working... or not")
