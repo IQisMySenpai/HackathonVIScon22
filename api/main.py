8d0d23898f4d0b21b4e1ed472a187bea50550a95
AliSot2000 <alisot200@gmail.com>
2022-10-15 19:13:56 +0200

    + Python conform syntax, Added /test-login functon

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request, Cookie
 from fastapi.staticfiles import StaticFiles
 
 from courses import *
@@ -7,25 +7,35 @@ from db import mongo
 app = FastAPI()
 api = FastAPI()
 
+
 @api.get("/tags")
 def get_tags(response: Response):
     return list_tags(mongo, response)
+
 
 @api.post("/tags")
 def post_tag(tag: Tag, response: Response):
     return create_tag(mongo, response, tag)
 
-#@api.get("/lecturers")
-#def read_lecturers(response: Response):
+
+# @api.get("/lecturers")
+# def read_lecturers(response: Response):
 #    return list_lecturers(mongo, response)
 
 @api.get("/courses")
 def read_courses(response: Response, page: int = 0):
     return list_courses(mongo, response, page)
 
+
 @api.post("/courses")
 def post_course(course: Course, response: Response):
     return create_course(mongo, response, course)
+
+
+@api.post("/test-login")
+def post_test_login(request: Request, response: Response):
+    return test_login(request, response)
+
 
 app.mount("/api", api)
 app.mount("/", StaticFiles(directory="../Webinterface", html=True), name="WebInterface")
