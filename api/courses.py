d9845cd2b6b692846688fbe5ae3efd48193431ea
AliSot2000 <alisot200@gmail.com>
2022-10-15 17:48:19 +0200

    + Pythonified file

--- api/courses.py
+++ api/courses.py
@@ -4,6 +4,7 @@ from mongo_api import *
 from api_common import *
 from structs import *
 
+
 def list_tags(db: MongoAPI, response: Response):
     tags = db.find("tags")
 
@@ -11,6 +12,7 @@ def list_tags(db: MongoAPI, response: Response):
         return pack_response(response, 204, "No tags found.")
 
     return pack_response(response, 200, "ok", {"tags": Tag.out(Tag.from_db(tags))})
+
 
 def create_tag(db: MongoAPI, response: Response, tag: Tag):
     tag.id = db.insert_one("tags", tag.db_dict())
@@ -20,6 +22,7 @@ def create_tag(db: MongoAPI, response: Response, tag: Tag):
 
     return pack_response(response, 200, "ok", {"id": tag.id.__str__()})
 
+
 def list_lecturers(db: MongoAPI, response: Response):
     profs = db.find("professors")
 
@@ -28,8 +31,9 @@ def list_lecturers(db: MongoAPI, response: Response):
 
     return pack_response(response, 200, "ok", {"lecturers": Lecturer.out(Lecturer.from_db(profs))})
 
+
 def list_courses(db: MongoAPI, response: Response, page):
-    courses = db.find("courses", skip= 20 * page, limit=20)
+    courses = db.find("courses", skip=20 * page, limit=20)
 
     if len(courses) == 0:
         return pack_response(response, 204, "No courses found.")
@@ -39,8 +43,8 @@ def list_courses(db: MongoAPI, response: Response, page):
 
     return pack_response(response, 200, "ok", {"courses": Course.out(courses)})
 
-def create_course(db: MongoAPI, response: Response, course: Course):
 
+def create_course(db: MongoAPI, response: Response, course: Course):
     # resolve tag data, resolve lecturers data
     if course.tags is not None:
         course.tags = Tag.from_db(db.find("tags", {'_id': {'$in': [ObjectId(tag.id) for tag in course.tags]}}))
@@ -54,11 +58,10 @@ def create_course(db: MongoAPI, response: Response, course: Course):
 
     return pack_response(response, 200, "ok", {"id": course.id.__str__()})
 
-def load_lecturer_for_courses(db: MongoAPI, courses: list[Course]):
+
+def load_lecturer_for_courses(db: MongoAPI, courses: List[Course]):
     for course in courses:
         if course.lecturers is None:
             continue
         print(course.lecturers)
         course.lecturers = Lecturer.from_db(db.find("professors", find_all_id_query(course.lecturers), limit=10))
-
-
