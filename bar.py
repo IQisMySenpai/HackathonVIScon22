a6ecdcbb61827e007e5433933d537daf751a7fda
AliSot2000 <alisot200@gmail.com>
2022-10-15 19:46:56 +0200

    + Removed Files which were used to give Emilis an introduction to python.

--- bar.py
+++
@@ -1,6 +0,0 @@
-from foo import printer
-
-if __name__ == "__main__":
-    print("My name is Cedric")
-    print("I Cedric all day every day")
-    printer()
--- foo.py
+++
@@ -1,7 +0,0 @@
-def printer():
-    print("My name is olaf")
-    print("I scholz all day every day")
-
-# if __name__ == "__main__":
-print(__name__)
-printer()
