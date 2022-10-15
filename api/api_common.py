7b12eb575298e53edbb2d7e611e977022103e1e1
nils <negger@student.ethz.ch>
2022-10-15 16:39:21 +0200

    tags can now be created and added to a course during its creation

--- api/api_common.py
+++ api/api_common.py
@@ -1,7 +1,8 @@
+from fastapi import Response
 
-
-def pack_response(status: int, message: str, data: dict = None):
+def pack_response(response: Response, status: int, message: str, data: dict = None):
     default = {"status": status, "msg": message}
+    response.status_code = status
     if data is None:
         return default
     else:
--- api/courses.py
+++ api/courses.py
@@ -1,12 +1,52 @@
+from fastapi import Response
+from bson.objectid import ObjectId
 from mongo_api import *
 from api_common import *
 from structs import *
 
-def get_courses(db: MongoAPI):
+def list_tags(db: MongoAPI, response: Response):
+    tags = db.find("tags")
+
+    if len(tags) == 0:
+        return pack_response(response, 204, "No tags found.")
+
+    return pack_response(response, 200, "ok", {"tags": Tag.out(Tag.from_db(tags))})
+
+def create_tag(db: MongoAPI, response: Response, tag: Tag):
+    tag.id = db.insert_one("tags", tag.db_dict())
+
+    if tag.id is None:
+        return pack_response(response, 400, "Failed to create tag.")
+
+    return pack_response(response, 200, "ok", {"id": tag.id.__str__()})
+
+def list_lecturers(db: MongoAPI, response: Response):
+    profs = db.find("professors")
+
+    if len(profs) == 0:
+        return pack_response(response, 204, "No Profs found.")
+
+    return pack_response(response, 200, "ok", {"lecturers": Lecturer.out(Lecturer.from_db(profs))})
+
+def list_courses(db: MongoAPI, response: Response):
     courses = db.find("courses")
 
     if len(courses) == 0:
-        return pack_response(204, "No courses found.")
+        return pack_response(response, 204, "No courses found.")
 
-    return pack_response(200, "ok", {"courses": Course.out(Course.from_db(courses))})
+    return pack_response(response, 200, "ok", {"courses": Course.out(Course.from_db(courses))})
+
+def create_course(db: MongoAPI, response: Response, course: Course):
+
+    # resolve tag data, resolve lecturers data
+    if course.tags is not None:
+        course.tags = Tag.from_db(db.find("tags", {'_id': {'$in': [ObjectId(tag.id) for tag in course.tags]}}))
+
+    course.id = db.insert_one("courses", course.db_dict())
+
+    if course.id is None:
+        return pack_response(response, 400, "Failed to create course")
+
+    return pack_response(response, 200, "ok", {"id": course.id.__str__()})
+
 
