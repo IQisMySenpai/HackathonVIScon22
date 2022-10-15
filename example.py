35bcb412af901252109f320af41deabb11edaf27
AliSot2000 <alisot200@gmail.com>
2022-10-15 11:24:35 +0200

    + Working Example Mongo db

--- example.py
+++ example.py
@@ -1,10 +1,9 @@
-import os
+from mongo_api import MongoAPI
 
-print(os.path.abspath(__name__))
+if __name__ == "__main__":
+    mongo = MongoAPI('vvzpp.5byhvi1.mongodb.net', 'VVZpp', 'server', 'Xr9wqtrvnlufZQho')
 
+    new_movie = {'name': 'First Man', 'length': 141}
+    res = mongo.insert_one('lectures', new_movie)
 
-
-
-
-
-print("Alex did some stuff which is working... or not")
+    print(res)
