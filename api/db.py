0989c8b5ca42620c0d5231fd6c5f5a54ac451fca
Jannick <jannick.schroer@gmail.com>
2022-10-17 14:13:55 +0200

    + Changed User Password

--- api/db.py
+++ api/db.py
@@ -1,3 +1,3 @@
 from mongo_api import *
 
-mongo = MongoAPI('vvzpp.5byhvi1.mongodb.net', 'VVZpp', 'admin', 'LR3I3ChKSA59lVmC')
+mongo = MongoAPI('vvzpp.5byhvi1.mongodb.net', 'VVZpp', 'admin', 'okcx7wmkYnzWTGtJ')
